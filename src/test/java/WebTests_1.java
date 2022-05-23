6a6f0c1048723426f6cc0168fc33951c836a09ec
dmswlpk <103320248+dmswlpk@users.noreply.github.com>
2022-05-22 20:48:10 -0600

    new changes

--- src/test/java/WebTests_1.java
+++ src/test/java/WebTests_1.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -42,10 +41,11 @@ public class WebTests_1 {
         WebDriver driver = new ChromeDriver();
 
         driver.get(url);
-        WebElement submitNewLanguage = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href=/submitnewlanguage.html]"));
+        WebElement submitNewLanguage = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/submitnewlanguage.html']"));
         String actualResult = submitNewLanguage.getText();
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(actualResult, expectedResult.toUpperCase());
 
         // driver.quit();
     }
@@ -55,7 +55,7 @@ public class WebTests_1 {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "C://chromeDriver.exe";
         String url = "http://www.99-bottles-of-beer.net/";
-        String expectedResult = "Submit new Language".toLowerCase();
+        String expectedResult = "Submit New Language";
 
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
@@ -69,12 +69,11 @@ public class WebTests_1 {
         WebElement submitNewLanguage = driver.findElement(
                 By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='./submitnewlanguage.html']"));
 
-        String actualResult = submitNewLanguage.getText().trim().toLowerCase();
-        Assert.assertEquals(actualResult, expectedResult.toLowerCase());
+        String actualResult = submitNewLanguage.getText().trim().toUpperCase();
+        Assert.assertEquals(actualResult, expectedResult.toUpperCase());
 
         driver.quit();
     }
-
 
     @Test
     public void TestCase_11_04_ConfirmTheSubtitle0_9() throws InterruptedException {
@@ -92,8 +91,8 @@ public class WebTests_1 {
         WebElement menuSubtitle0_9 = driver.findElement(
                 By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='0.html']".trim()));
 
-        //WebElement menuSubtitle0_9 = driver.findElement(
-        //        By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[text()='0-9']"));
+        // WebElement menuSubtitle0_9 = driver.findElement(
+        //         By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[text()='0-9']"));
         String actualResult = menuSubtitle0_9.getText();
         Thread.sleep(2000);
 
@@ -111,7 +110,9 @@ public class WebTests_1 {
         String expectedResult1 = "Oliver Schade";
         String expectedResult2 = "Gregor Scheithauer";
         String expectedResult3 = "Stefan Scheler";
-        final String expectedResult = "Oliver Schade, Gregor Scheithauer, Stefan Scheler".trim();
+        final String[] expectedResult = {"Oliver Schade" +
+                "Gregor Scheithauer",
+                "Stefan Scheler"};
 
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
@@ -121,28 +122,30 @@ public class WebTests_1 {
                 By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='team.html']"));
         menuTeam.click();
 
-        //WebElement creatorNames = driver.findElement(
-        //        By.xpath("//body/div[@id='wrap']//div[@id='main']/h3[text()='Oliver Schade']"));
+        WebElement creatorNames = driver.findElement(
+                By.xpath("//body/div[@id='wrap']//div[@id='main']/h3[text()='Oliver Schade']"));
 
-        //String actualResult1 = creatorNames.getText();
+        String actualResult1 = creatorNames.getText();
 
-        //WebElement creatorNames2 = driver.findElement(
-        //        By.xpath("//body/div[@id='wrap']//div[@id='main']/h3[text()='Gregor Scheithauer']"));
-        //String actualResult2 = creatorNames2.getText();
-        //WebElement creatorNames3 = driver.findElement(
-        //        By.xpath("//body/div[@id='wrap']//div[@id='main']/h3[text()='Stefan Scheler']"));
-        //String actualResult3 = creatorNames3.getText();
+        WebElement creatorNames2 = driver.findElement(
+                By.xpath("//body/div[@id='wrap']//div[@id='main']/h3[text()='Gregor Scheithauer']"));
+        String actualResult2 = creatorNames2.getText();
+        WebElement creatorNames3 = driver.findElement(
+                By.xpath("//body/div[@id='wrap']//div[@id='main']/h3[text()='Stefan Scheler']"));
+        String actualResult3 = creatorNames3.getText();
 
-        WebElement names = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='main']//[*]"));
+           WebElement[] names = new WebElement[3];
+           for(int i = 0; i < names.length; i++){
+              if(names[i].equals(driver.findElement(
+                       By.xpath("//body/div[@id='wrap']/div[@id='main']/h3"))));
+           }
 
-        String actualResult = names.getText();
 
-        //Assert.assertEquals(actualResult1, expectedResult1);
-        //Assert.assertSame(actualResult2, expectedResult2);
-        //Assert.assertSame(actualResult3, expectedResult3);
+        //   String actualResult = names.getText();
 
-        Assert.assertEquals(actualResult, expectedResult);
+        //Assert.assertEquals;
+        Assert.assertEquals(actualResult2, expectedResult2);
+        Assert.assertEquals(actualResult3, expectedResult3);
 
         driver.quit();
     }
@@ -229,6 +232,8 @@ public class WebTests_1 {
         String url = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
         String expectedResult = "Error: Precondition failed - Incomplete Input.";
 
+       // String [] expectedResult = expectedResult.split(" ");
+
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
 
@@ -236,10 +241,10 @@ public class WebTests_1 {
         driver.manage().window().fullscreen();
 
         WebElement sumbitLngsBtnk = driver.findElement(
-              By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@name='submitlanguage']"
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@name='submitlanguage']"
                 )
         );
-       // Thread.sleep(1000);
+        // Thread.sleep(1000);
 
         Robot robot = new Robot();
         robot.keyPress(KeyEvent.VK_PAGE_DOWN);
@@ -249,15 +254,17 @@ public class WebTests_1 {
 
         WebElement findErrorMsg = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/p"));
 
-
-
         WebElement findError = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='main']/p//[contains(text(),'Error']"));
-        String actualResult = findError.getText();
-      /**  /E[contains(concat('
-                ⦿', @A, '⦿'), '⦿w⦿')*/
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/p//[contains(text(),'Error: Precondition failed - Incomplete Input.']"));
+        //String[] arrOfWordsFromError = findError.getText().split();
+        /**  /E[contains(concat('
+         ⦿', @A, '⦿'), '⦿w⦿')*/
 
-        Assert.assertEquals(actualResult, expectedResult);
+        if (findError.getText().contains("E")) {
+
+        }
+
+       // Assert.assertEquals(actualResult, expectedResult);
 
         driver.quit();
     }
@@ -285,7 +292,7 @@ public class WebTests_1 {
 
         Assert.assertEquals(actualResult, expectedResult);
 
-        driver.quit();
+        //    driver.quit();
     }
 
     @Test
@@ -299,15 +306,18 @@ public class WebTests_1 {
         WebDriver driver = new ChromeDriver();
 
         driver.get(url);
-        WebElement browseLnggs = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']"));
+        WebElement browseLnggs = driver.
+                findElement(
+                        By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']"));
 
         browseLnggs.click();
 
-        WebElement table1_Names = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='main']/table[@id='category']/tbody/tr/th[text()='Language']"));
-        WebElement table2_Names = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='main']/table[@id='category']/tbody/tr/th[text()='Author']"));
+        WebElement table1_Names = driver.
+                findElement(
+                        By.xpath("//body/div[@id='wrap']/div[@id='main']/table[@id='category']/tbody/tr/th[text()='Language']"));
+        WebElement table2_Names = driver.
+                findElement(
+                        By.xpath("//body/div[@id='wrap']/div[@id='main']/table[@id='category']/tbody/tr/th[text()='Author']"));
 
         String[] actualResult = {table1_Names.getText(), table2_Names.getText()};
 
@@ -323,21 +333,21 @@ public class WebTests_1 {
         String url = "http://www.99-bottles-of-beer.net/";
         String expectedResult = "";
 
-        System.setProperty(chromeDriver,driverPath);
+        System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
         driver.get(url);
         WebElement topList = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[text()='Top Lists']"));
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/toplist.html']"));
         topList.click();
 
         WebElement newComments = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[text()='New Comments']"));
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='./newcomments.html']"));
         newComments.click();
         WebElement isNewCommentsEmpty = driver.findElement(
                 By.xpath("//body/div[@id='wrap']/div[@id='main']/h2[text()='New Comments']/following-sibling::p[1]"));
         String actualResult = isNewCommentsEmpty.getText();
-       // if(isNewCommentsEmpty.getText().equals(expectedResult))
-        Assert.assertEquals(actualResult,expectedResult);
+        // if(isNewCommentsEmpty.getText().equals(expectedResult))
+        Assert.assertEquals(actualResult, expectedResult);
 
         driver.quit();
     }
@@ -348,21 +358,20 @@ public class WebTests_1 {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "C://chromeDriver.exe";
         String url = "http://www.99-bottles-of-beer.net/submitnewlanguage.html ";
-        String expectedResult = "700rgba(255, 255, 255, 1)";
+        String expectedResult = "IMPORTANT";
 
-        System.setProperty(chromeDriver,driverPath);
+        System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
 
         driver.get(url);
-        WebElement importantMsg = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='main']/ul/li/span/b[text()='IMPORTANT:']"));
+        String bold = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/ul/li/span[@style='background-color:red;color: white']/b']")).getAttribute("style");
 
-        importantMsg.isDisplayed();
 
-        String actualResult = importantMsg.getCssValue("font-weight").concat(importantMsg.getCssValue("color"));
-        String actualResult1= importantMsg.getCssValue("font color");
-
-        Assert.assertEquals(actualResult1,expectedResult);
+        //String actualResult = importantMsg.getCssValue("font-weight").concat(importantMsg.getCssValue("color"));
+    //  /String actualResult1 = bold.getCssValue("font color");
+//
+    //   Assert.assertEquals(, expectedResult);
 
         driver.quit();
     }
